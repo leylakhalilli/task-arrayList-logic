37abb14604e70c2b67e26d5c9d90a7e5d2142a88
leylakhalilli <leylaxelilli65@gmail.com>
2022-06-29 20:19:57 +0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        MyArrayList arrayList = new MyArrayList();
+//        ArrayList list=new ArrayList();
+//        list.add(4);
+        arrayList.add("leyla");
+        arrayList.add(6);
+        arrayList.add(7);
+        arrayList.add(8);
+        arrayList.add(9);
+        for (int i = 0; i < arrayList.getSize(); i++) {
+            System.out.println(arrayList.index(i));
+        }
+    }
+}
---
+++ src/MyArrayList.java
@@ -0,0 +1,29 @@
+public class MyArrayList<Y> {
+    private int index = 0;
+    private Y[] arr;
+//   private double capacity=1.5;
+
+    public MyArrayList() {
+        arr = (Y[]) new Object[3];
+    }
+
+    public void add(Y newElement) {
+        arr[index++] = newElement;
+        if (index >= arr.length) {
+            Y newArray[] = (Y[]) new Object[arr.length + 1];
+            for (int i = 0; i < arr.length; i++) {
+                newArray[i] = arr[i];
+            }
+            arr = newArray;
+        }
+    }
+
+    public int getSize() {
+        return index;
+    }
+
+    public Y index(int index) {
+        return arr[index];
+    }
+}
+
